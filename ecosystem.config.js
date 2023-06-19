be83cb1eb2eb4f07c3fc6de996659bc98f89138a
alesikivan <obcofficiall@gmail.com>
2023-06-19 22:48:46 +0300

    added new client app build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,24 @@
 module.exports = {
-  apps : [{
-    script: './server.js',
-    env : {
-      'PORT': 4000,
-      'NODE_ENV' : 'production'
+  apps: [
+    {
+      name: 'server',
+      script: './server.js',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4000
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'root',
-      host : ['92.118.150.71'],
-      ref  : 'origin/main',
-      repo : 'https://github.com/alesikivan/data-analytics-server',
-      path : '/root/apps/digital-city',
+  deploy: {
+    production: {
+      user: 'root',
+      host: ['92.118.150.71'],
+      ref: 'origin/main',
+      repo: 'https://github.com/alesikivan/data-analytics-server',
+      path: '/root/apps/digital-city',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 delete server && pm2 start server.js && pm2 save --force',
+      'post-deploy': 'npm install && pm2 delete server && pm2 start server.js && pm2 save --force',
       'pre-setup': ''
     }
   }
