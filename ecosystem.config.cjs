97eb3ae2b04d86f60f292449c7e0f785de65bd67
alesikivan <obcofficiall@gmail.com>
2023-06-23 14:44:30 +0300

    fix (ecosystem config): changed the git repo path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
       user: 'ialesik',
       host: ['mda.math.muni.cz'],
       ref: 'origin/main',
-      repo: 'git@github.com:alesikivan/data-analytics-server.git',
+      repo: 'https://github.com/alesikivan/data-analytics-server',
       path: '/home/ialesik/apps/digital-city',
       'pre-deploy-local': '',
       'post-deploy': 'npm install && pm2 delete data-analytics-server && pm2 start server.js && pm2 save --force',
