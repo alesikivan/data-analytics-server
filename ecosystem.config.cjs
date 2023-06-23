f760ac055a6f8f7d38cbac9413ec207c308369a9
alesikivan <obcofficiall@gmail.com>
2023-06-23 15:30:15 +0300

    changed the ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -18,7 +18,7 @@ module.exports = {
       repo: 'https://github.com/alesikivan/data-analytics-server',
       path: '/home/ialesik/apps/digital-city',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 delete data-analytics-server && pm2 start server.js --name "data-analytics-server" && pm2 save --force',
+      'post-deploy': 'npm install && pm2 delete data-analytics-server && pm2 start server.js --name data-analytics-server && pm2 save --force',
       'pre-setup': ''
     }
   }
