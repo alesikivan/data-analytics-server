9a8674ee23eeaee5f108d0f78d5f9b0fc2e7e38d
alesikivan <obcofficiall@gmail.com>
2024-05-09 20:02:11 +0300

    ecosystem update

---
+++ ecosystem.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: 'data-analytics-server',
+      script: './server.js',
+      ignore_watch: [
+        'db',
+      ],
+      env: {
+        NODE_ENV: 'production',
+        PORT: 5000
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'ialesik',
+      host: ['mda.math.muni.cz'],
+      ref: 'origin/main',
+      repo: 'https://github.com/alesikivan/data-analytics-server',
+      path: '/home/ialesik/apps/digital-city',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 delete data-analytics-server && --watch --ignore-watch="db" --name data-analytics-server && pm2 save --force',
+      'pre-setup': ''
+    }
+  }
+}
+
